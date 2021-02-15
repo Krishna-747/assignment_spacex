fb29058f2e83d13016a1e546e4cbc966863c2b92
Krishna-747 <krishnachaitanya125@gmail.com>
2021-02-15 16:18:08 +0530

    'SSR'

--- server/server.js
+++ server/server.js
@@ -7,33 +7,35 @@ import ReactDOMServer from 'react-dom/server'
 
 import App from '../src/App'
 
-const PORT = 8080
+const PORT = 8000
 const app = express()
 
 const router = express.Router()
 
-const serverRender = (req, res, next) => {
-    fs.readFile(path.resolve('.build/index.hmtl'), 'utf-8', (err, data) => {
-        if(err){
-            console.log(err)
-            return res.status(500).send('An error occured')
-        }
-        return res.send(
-            data.replace(
-                '<div id="root"></div>',
-                `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
-            )
-        )
-    })
+const serverRenderer = (req, res, next) => {
+  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send('An error occurred')
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    )
+  })
 }
 router.use('^/$', serverRenderer)
 
 router.use(
-    express.static(path.resolve(__dirname, '..', 'build')), { maxAge: '30d' }
+  express.static(path.resolve(__dirname, '..', 'build'))
 )
 
+// tell the app to use the above rules
 app.use(router)
 
+// app.use(express.static('./build'))
 app.listen(PORT, () => {
-    console.log(`SSR running on port ${PORT}`)
+  console.log(`SSR running on port ${PORT}`)
 })
